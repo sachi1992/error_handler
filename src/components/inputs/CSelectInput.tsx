d2a42438b37396005cb6193c0a7d64567cc1a850
Amanda Warnakula <sachiniamanda1490@gmail.com>
2024-03-29 20:23:14 +0530

    add custom select inputs

--- src/components/inputs/CSelectInput.tsx
+++ src/components/inputs/CSelectInput.tsx
@@ -47,12 +47,28 @@ const CSelectInput: React.FC<BaseICSelectInputProps> = (props) => {
   ${isError ? "focus:border-red-300" : "focus:border-green-300"} 
   disabled:border-gray-300`;
 
+  const dropdownIconStyle = {
+    appearance: "none",
+    WebkitAppearance: "none",
+    MozAppearance: "none",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "right 8px center",
+    backgroundSize: "24px",
+    paddingRight: "16px",
+    backgroundImage:
+      "url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='rgba(0,0,0,0.4)' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><polyline points='6 9 12 15 18 9'></polyline></svg>\")",
+  };
+
   return (
     <div>
       <label htmlFor={props.id || props.name}>{props.label}</label>
       <br />
 
-      <Field as="select" className={inputClass} {...props}>
+      <Field
+        as="select"
+        className={inputClass}
+        style={dropdownIconStyle}
+        {...props}>
         <option key="select" value="">
           Select an option
         </option>
